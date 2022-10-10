857a7c91160eb88e24669258b5a3e0995dd995a7
KYR <freekyr7529@gmail.com>
2022-10-10 23:35:55 +0900

    Initial commit

---
+++ src/main/java/BaekJoon1037.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon1037 {
+    static int N;
+    static char[] answer, nameToCompare;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        answer = br.readLine().toCharArray();
+
+        for (int i = 0; i < N - 1; i++) {
+            nameToCompare = br.readLine().toCharArray();
+            for (int j = 0; j < answer.length; j++) {
+                if(answer[j] != nameToCompare[j]) {
+                    answer[j] = '?';
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
