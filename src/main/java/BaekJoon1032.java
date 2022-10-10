f774fb8a19bc2bfd4c3796e1f57ded3cc0aecad0
KYR <freekyr7529@gmail.com>
2022-10-10 23:39:12 +0900

    setting

--- src/main/java/BaekJoon1032.java
+++ src/main/java/BaekJoon1032.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BaekJoon1037 {
+public class BaekJoon1032 {
     static int N;
     static char[] answer, nameToCompare;
     public static void main(String[] args) throws IOException {
