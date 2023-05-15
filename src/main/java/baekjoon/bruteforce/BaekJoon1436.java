21b5a31e07df3db5ad92e5fed085d2c501b90db7
Yeong Ryeol Kim <freekyr7529@gmail.com>
2023-05-15 17:51:47 +0900

    feat: update

---
+++ src/main/java/baekjoon/bruteforce/BaekJoon1436.java
@@ -0,0 +1,24 @@
+package baekjoon.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon1436 {
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+        int cnt = 0;
+        int title = 666;
+
+        while (true) {
+            String tmp = String.valueOf(title);
+            if (tmp.contains("666")) {
+                cnt++;
+                if (cnt == n) {
+                    System.out.println(title);
+                    break;
+                }
+            }
+            title++;
+        }
+    }
+}
