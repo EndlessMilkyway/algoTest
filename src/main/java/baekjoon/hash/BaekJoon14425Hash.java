8021353dabf3fe41b491a8336b63ac4c9c64220e
Yeong Ryeol Kim <freekyr7529@gmail.com>
2023-05-16 23:57:20 +0900

    feat: 14425번 풀이
    트리, 해시

---
+++ src/main/java/baekjoon/hash/BaekJoon14425Hash.java
@@ -0,0 +1,28 @@
+package baekjoon.hash;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BaekJoon14425Hash {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++)
+            set.add(br.readLine());
+
+        int cnt = 0;
+        for (int i = 0; i < m; i++) {
+            if (set.contains(br.readLine()))
+                cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
