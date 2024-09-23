25479a7c4098f6a32d3ed290e90e888e00647c5d
Kim Yeong Ryeol <freekyr7529@gmail.com>
2024-09-23 16:16:11 +0900

    feat: 백준 2164번 다시 풀이
    카드2
    
    #큐

---
+++ src/main/java/baekjoon/queue/BaekJoon2164_RE.java
@@ -0,0 +1,25 @@
+package baekjoon.queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BaekJoon2164_RE {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i < n + 1; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.poll());
+    }
+}
